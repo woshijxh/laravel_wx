b1d960474d22480a3a1c6f1846d7b732074d744b
jxh <1059751314@qq.com>
2019-04-24 10:10:09 +0800

    user pemrs

--- src/mixins/replyMixin.js
+++ src/mixins/replyMixin.js
@@ -65,7 +65,7 @@ export default class ReplyMixin extends wepy.mixin {
     if (!user) {
       return false
     }
-    return (reply.user_id === user.id)
+    return (reply.user_id === user.id) || this.$parent.can('manage_contents')
   }
   async onPullDownRefresh() {
     this.noMoreData = false
